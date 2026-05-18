f91f8ff0fb2c8d131d1b08793d819ecff0a6e953
Vinodhini.J <jvinodhini006@gmail.com>
2026-05-18 10:19:25 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Maven");
+        System.out.println("This is the simple realworld example....");
+
+        int a = 5;
+        int b = 10;
+        System.out.println("Sum of " + a + " and " + b + " is " + sum(a, b));
+    }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+}
